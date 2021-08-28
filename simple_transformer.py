0a08ddee327c7bdbaa84ac006112a118b3537f1c
sabernn <60275211+sabernn@users.noreply.github.com>
2021-08-28 18:28:29 -0500

    implementing the Transformer

--- simple_transformer.py
+++ simple_transformer.py
@@ -7,6 +7,13 @@ from skimage.io import imread, imshow
 import matplotlib.pyplot as plt
 
 
+def scaled_dot_product_attention(query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> Tensor:
+    temp=query.bmm(key.transpose(1,2))
+    scale=query.size(-1) ** 0.5
+    softmax = F.softmax(temp / scale, dim=-1)
+    return softmax.bmm(value)
+
+
 class AttentionHead(nn.Module):
     def __init__(self,dim_in: int, dim_k: int, dim_v: int):
         super().__init__()
@@ -39,21 +46,180 @@ def position_encoding(
 
     return torch.where(dim.long() % 2 == 0, torch.sin(phase), torch.cos(phase))
 
+def feed_forward(dim_input: int = 512, dim_feedforward: int = 2048) -> nn.Module:
+    return nn.Sequential(
+        nn.Linear(dim_input, dim_feedforward),
+        nn.ReLU(),
+        nn.Linear(dim_feedforward,dim_input),
+    )
+
+class Residual(nn.Module):
+    def __init__(self, sublayer: nn.Module, dimension: int, dropout: float = 0.1):
+        super().__init__()
+        self.sublayer = sublayer
+        self.norm = nn.LayerNorm(dimension)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, *tensors: torch.Tensor) -> torch.Tensor:
+        return self.norm(tensors[-1] + self.dropout(self.sublayer(*tensors)))
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(
+        self,
+        dim_model: int = 512,
+        num_heads: int = 6,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+    ):
+        super().__init__()
+        dim_k = dim_v = dim_model // num_heads
+        self.attention = Residual(
+            MultiHeadAttention(num_heads, dim_model, dim_k, dim_v),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+        self.feed_forward = Residual(
+            feed_forward(dim_model, dim_feedforward),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+
+    def forward(self, src: torch.Tensor) -> torch.Tensor:
+        src = self.attention(src, src, src)
+        return self.feed_forward(src)
+
+class TransformerEncoder(nn.Module):
+    def __init__(
+        self,
+        num_layers: int = 6,
+        dim_model: int = 512,
+        num_heads: int = 8,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            TransformerEncoderLayer(dim_model, num_heads, dim_feedforward, dropout)
+            for _ in range(num_layers)
+        ])
+
+    def forward(self, src: torch.Tensor) -> torch.Tensor:
+        seq_len, dimension = src.size(1), src.size(2)
+        src += position_encoding(seq_len, dimension)
+        for layer in self.layers:
+            src = layer(src)
+
+        return src
+
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(
+        self, 
+        dim_model: int = 512, 
+        num_heads: int = 6, 
+        dim_feedforward: int = 2048, 
+        dropout: float = 0.1, 
+    ):
+        super().__init__()
+        dim_k = dim_v = dim_model // num_heads
+        self.attention_1 = Residual(
+            MultiHeadAttention(num_heads, dim_model, dim_k, dim_v),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+        self.attention_2 = Residual(
+            MultiHeadAttention(num_heads, dim_model, dim_k, dim_v),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+        self.feed_forward = Residual(
+            feed_forward(dim_model, dim_feedforward),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+
+    def forward(self, tgt: torch.Tensor, memory: torch.Tensor) -> torch.Tensor:
+        tgt = self.attention_1(tgt, tgt, tgt)
+        tgt = self.attention_2(memory, memory, tgt)
+        return self.feed_forward(tgt)
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(
+        self, 
+        num_layers: int = 6,
+        dim_model: int = 512, 
+        num_heads: int = 8, 
+        dim_feedforward: int = 2048, 
+        dropout: float = 0.1, 
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            TransformerDecoderLayer(dim_model, num_heads, dim_feedforward, dropout)
+            for _ in range(num_layers)
+        ])
+        self.linear = nn.Linear(dim_model, dim_model)
+
+    def forward(self, tgt: torch.Tensor, memory: torch.Tensor) -> torch.Tensor:
+        seq_len, dimension = tgt.size(1), tgt.size(2)
+        tgt += position_encoding(seq_len, dimension)
+        for layer in self.layers:
+            tgt = layer(tgt, memory)
+
+        return torch.softmax(self.linear(tgt), dim=-1)
+
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self, 
+        num_encoder_layers: int = 6,
+        num_decoder_layers: int = 6,
+        dim_model: int = 512, 
+        num_heads: int = 6, 
+        dim_feedforward: int = 2048, 
+        dropout: float = 0.1, 
+        activation: nn.Module = nn.ReLU(),
+    ):
+        super().__init__()
+        self.encoder = TransformerEncoder(
+            num_layers=num_encoder_layers,
+            dim_model=dim_model,
+            num_heads=num_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+        )
+        self.decoder = TransformerDecoder(
+            num_layers=num_decoder_layers,
+            dim_model=dim_model,
+            num_heads=num_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+        )
+
+    def forward(self, src: torch.Tensor, tgt: torch.Tensor) -> torch.Tensor:
+        return self.decoder(tgt, self.encoder(src))
+
+src = torch.rand(64, 16, 512)
+tgt = torch.rand(64, 16, 512)
+out = Transformer()(src, tgt)
+print(out.shape)
+
+
 
 batch_size=1
 seq_length=2000
 num_features=2000
+
+
 
 Attention=AttentionHead(seq_length,seq_length,seq_length)
 
 img=imread('zeiss.tiff')
 
 
-def scaled_dot_product_attention(query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> Tensor:
-    temp=query.bmm(key.transpose(1,2))
-    scale=query.size(-1) ** 0.5
-    softmax = F.softmax(temp / scale, dim=-1)
-    return softmax.bmm(value)
+
 
 
 query=torch.rand([batch_size,seq_length,num_features],dtype=torch.float)
